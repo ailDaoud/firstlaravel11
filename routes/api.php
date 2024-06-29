5442b90326de4ae5d66bf6ef4a25f446d4977960
ailDaoud <daoudali259@gmail.com>
2024-06-29 11:05:41 +0300

    3.6

--- routes/api.php
+++ routes/api.php
@@ -4,34 +4,5 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
-
-Route::controller(UserController::class)->group(function () {
-    Route::get('/showusers', 'index');
-    Route::post('/store_user', 'store');
-    Route::get('/user/show/{id}', 'show');
-    Route::post('/user/update/{id}', 'update');
-    Route::delete('/user/delete/{id}', 'destroy');
-});
-
-
-Route::get('home', [AuthController::class, 'home'])->name('home');
-Route::group([
-    'middleware' => 'auth',
-    'prefix' => 'auth'
-], function ($router) {
-    Route::match(['get', 'post'], 'logout', [AuthController::class, 'logout'])->name('logout');
-    Route::get('home', [AuthController::class, 'home'])->name('home');
-    Route::match(['get', 'post'], 'profile', [AuthController::class, 'profile'])->name('profile');
-});
-Route::group([
-    //   'middleware' => ['guest','web'],
-    'prefix' => 'auth'
-], function ($router) {
-    Route::match(['get', 'post'], 'register', [AuthController::class, 'register'])->name('register');
-    Route::match(['get', 'post'], 'login', [AuthController::class, 'login'])->name('login');
-    Route::match(['get', 'post'], 'verify_otp', [AuthController::class, 'verify_otp'])->name('verify_otp');
-});
+use App\Http\Controllers\AdsController;
+use App\Http\Middleware\Localize;
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,43 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\User;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AdsController;
 use App\Http\Middleware\Localize;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:api');
+
+Route::controller(UserController::class)->group(function () {
+    Route::get('/showusers', 'index');
+    Route::post('/store_user', 'store');
+    Route::get('/user/show/{id}', 'show');
+    Route::post('/user/update/{id}', 'update');
+    Route::delete('/user/delete/{id}', 'destroy');
 });
+
+
+Route::get('home', [AuthController::class, 'home'])->name('home');
+Route::group([
+    'middleware' => 'auth',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::match(['get', 'post'], 'logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('home', [AuthController::class, 'home'])->name('home');
+    Route::match(['get', 'post'], 'profile', [AuthController::class, 'profile'])->name('profile');
+});
+Route::group([
+    //   'middleware' => ['guest','web'],
+    'prefix' => 'auth'
+], function ($router) {
+    Route::match(['get', 'post'], 'register', [AuthController::class, 'register'])->name('register');
+    Route::match(['get', 'post'], 'login', [AuthController::class, 'login'])->name('login');
+    Route::match(['get', 'post'], 'verify_otp', [AuthController::class, 'verify_otp'])->name('verify_otp');
+});
+
 Route::middleware(Localize::class)->group(function(){
     Route::resource('users',UserController::class);
 });
